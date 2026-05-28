33ff397fa8dea2c38b8c98f67d2d79db6f6ab8d2
franciscokjm <kellitonfrancisco@gmail.com>
2026-05-28 14:40:46 -0300

    teste driver ok

--- PrimoVert/src/main.cpp
+++ PrimoVert/src/main.cpp
@@ -13,30 +13,30 @@ Developed by the Scientific Maintenance and Instrumentation Division - BMC
 
 #include <Arduino.h>
 
-
 //pins declatarions:
-#define butLight1_Pin 15 //button light ON / OFF 
+#define len_Pin 3 //Enable counter clockwise > Enable in HIGH
+#define ren_Pin 4 //Enable clockwise > Enable in HIGH
+#define lpwm_Pin 5 //Intensity control to L channel
+#define rpwm_Pin 6 //Intensity control to R channel
+#define aux1_Pin 7 //Enable AUX port 8 (J15-P2)
+#define led0_Pin 8 //Enable Led 1 indicator from intensity of light
+#define led4_Pin 9 //Enable Led 5 indicator from intensity of light
+#define led3_Pin 10 //Enable Led 4 indicator from intensity of light
+#define led2_Pin 11 //Enable Led 3 indicator from intensity of light
+#define led1_Pin 12 //Enable Led 2 indicator from intensity of light
+#define aux2_Pin 13 //Enable AUX port 7 (J15-P1)
 #define butLight2_Pin 14 //button light ON / OFF
-#define ris_Pin PC5 //read pin for current sensor R channel
+#define butLight1_Pin 15 //button light ON / OFF
+#define ris_Pin A5 //read pin for current sensor R channel
 #define lis_Pin A6 //read pin for current sensor L channel
 #define potLine_Pin A7 //read pin for adjust light
-#define len_Pin PD3 //Enable counter clockwise > Enable in HIGH
-#define ren_Pin PD4 //Enable clockwise > Enable in HIGH
-#define lpwm_Pin PD5 //Intensity control to L channel
-#define rpwm_Pin PD6 //Intensity control to R channel
-#define led0_Pin 8 //Enable Led 1 indicator from intensity of light
-#define led1_Pin 12 //Enable Led 2 indicator from intensity of light
-#define led2_Pin 11 //Enable Led 3 indicator from intensity of light
-#define led3_Pin 10 //Enable Led 4 indicator from intensity of light
-#define led4_Pin 9 //Enable Led 5 indicator from intensity of light
-#define aux1_Pin 7 //Enable AUX port 8 (J15-P2)
-#define aux2_Pin 13 //Enable AUX port 7 (J15-P1)
 
 //Variables declarations:
 boolean flagLight = false;
 byte buttonON_1;
 byte buttonON_2;
 int flag = 0;
+int lis, ris, adjLamp, adjLampMap;
 
 //Status of Microscope ON
 void blinkLed(){
@@ -61,37 +61,50 @@ bool buttonON(){
   else return false;
 }
 
+//Current Driver Read
+void currentRead(){
+  lis = analogRead(lis_Pin);
+  ris = analogRead(ris_Pin);
+}
+
+void adjustLamp(){
+  int val = analogRead(potLine_Pin);
+  val = map(val,0,1023,255,0);
+  adjLamp = val;
+}
+
 void setup() {
 
   Serial.begin(9600);
+  for (int i=3; i<16; i++){
+    pinMode(i,OUTPUT);
+  }
   pinMode(butLight1_Pin, INPUT_PULLUP);
   pinMode(butLight2_Pin, INPUT_PULLUP);
-  pinMode(ris_Pin,INPUT);
-  pinMode(lis_Pin,INPUT);
   pinMode(potLine_Pin,INPUT);
-  pinMode(len_Pin,OUTPUT);
-  pinMode(ren_Pin,OUTPUT);
-  pinMode(lpwm_Pin,OUTPUT);
-  pinMode(rpwm_Pin,OUTPUT);
-  pinMode(led0_Pin,OUTPUT);
-  pinMode(led1_Pin,OUTPUT);
-  pinMode(led2_Pin,OUTPUT);
-  pinMode(led3_Pin,OUTPUT);
-  pinMode(led4_Pin,OUTPUT);
-  pinMode(aux1_Pin,OUTPUT);
-  pinMode(aux2_Pin,OUTPUT);
 
- //Status of Microscope ON
+  //Settings of the driver lamp
+
+  for (int i=7; i<16; i++){
+    digitalWrite(i,LOW);
+  }
+  digitalWrite(len_Pin,HIGH); //Enable L channel
+  digitalWrite(ren_Pin,LOW); //Disable R channel
+  analogWrite(rpwm_Pin,0); //R Channel Off from PWM control
+
+  //Status of Microscope ON
   blinkLed();
 }
 
 
 void loop() {
-
-Serial.println (buttonON());
-
-
-
-  delay(500);
-
+  //Serial.println (buttonON());
+  
+  adjustLamp();
+  Serial.print("adjLamp: ");
+  Serial.println(adjLampMap);
+  //analogWrite(lpwm_Pin,adjLampMap);
+  analogWrite(lpwm_Pin,255);
+  
+delay(5000);
 }
