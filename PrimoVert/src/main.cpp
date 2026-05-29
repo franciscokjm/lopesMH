079738efe980e27c79f99f5fc79539b61f11972a
franciscokjm <kellitonfrancisco@gmail.com>
2026-05-29 17:04:02 -0300

    Firmware testado; acertar documentação.

--- PrimoVert/src/main.cpp
+++ PrimoVert/src/main.cpp
@@ -19,11 +19,11 @@ Developed by the Scientific Maintenance and Instrumentation Division - BMC
 #define lpwm_Pin 5 //Intensity control to L channel
 #define rpwm_Pin 6 //Intensity control to R channel
 #define aux1_Pin 7 //Enable AUX port 8 (J15-P2)
-#define led0_Pin 8 //Enable Led 1 indicator from intensity of light
-#define led4_Pin 9 //Enable Led 5 indicator from intensity of light
-#define led3_Pin 10 //Enable Led 4 indicator from intensity of light
-#define led2_Pin 11 //Enable Led 3 indicator from intensity of light
-#define led1_Pin 12 //Enable Led 2 indicator from intensity of light
+#define led0_Pin 8 //Enable Led 1 indicator of Microscope ON
+#define led1_Pin 9 //Enable Led 2 indicator from intensity of light
+#define led2_Pin 10 //Enable Led 3 indicator from intensity of light
+#define led3_Pin 11 //Enable Led 4 indicator from intensity of light
+#define led4_Pin 12 //Enable Led 5 indicator from intensity of light
 #define aux2_Pin 13 //Enable AUX port 7 (J15-P1)
 #define butLight2_Pin 14 //button light ON / OFF
 #define butLight1_Pin 15 //button light ON / OFF
@@ -36,25 +36,65 @@ bool lightState = LOW;
 bool readButton_1, readButton_2;
 bool lastButState_1, lastButState_2 = LOW;
 
-
+long int lis, ris;
 int readButton;
-int lis, ris, adjLamp, adjLampMap;
-int dlyLamp = 2000;
+int adjLamp, adjLampMap;
+int dlyLamp = 1;
 
 //Status of Microscope ON
 void blinkLed(){
-  for(int i=0; i<2; i++){
-    for (int i=8; i<13; i++){
-      digitalWrite(i,HIGH);
-    }
-    delay(3000);
-    for (int i=8; i<13; i++){
-      digitalWrite(i,LOW);
-    }
-    delay(3000);
+  for (int i=8; i<13; i++){
+    digitalWrite(i,HIGH);
+    delay(10);
   }
+  delay(3000);
+  for (int i=9; i<13; i++){
+    digitalWrite(i,LOW);
+    delay(10);
+  }
+  delay(3000);
 }
 
+void standbyBlink(){
+  digitalWrite(led0_Pin,LOW);
+  delay(500);
+  digitalWrite(led0_Pin,HIGH);
+  delay(500);
+}
+void currentLed(int valor){
+  if (valor < 51) {
+    for (int i=12; i>8; i--){ //desliga 12, 11, 10, 9
+      digitalWrite(i,LOW);
+    }
+  }
+  if (valor > 50 && valor < 102){
+     for (int i=12; i>11; i--){ //Liga 12
+      digitalWrite(i,HIGH);
+    }
+    for (int i=11; i>8; i--){ //desliga 11, 10, 9
+      digitalWrite(i,LOW);
+    }
+  }
+  if (valor > 101 && valor < 153){
+     for (int i=12; i>10; i--){ //Liga 12, 11
+      digitalWrite(i,HIGH);
+    }
+    for (int i=10; i>8; i--){ //desliga 10, 9
+      digitalWrite(i,LOW);
+    }
+  }
+  if (valor > 152 && valor < 204){
+     for (int i=12; i>9; i--){ //Liga 12, 11, 10
+      digitalWrite(i,HIGH);
+    }
+    digitalWrite(9,LOW); //desliga 9
+  }
+  if (valor > 203 && valor < 256){
+     for (int i=12; i>8; i--){ //Liga 12, 11, 10, 9
+      digitalWrite(i,HIGH);
+    }
+  }
+}
 //Read buttons Auto
 void buttonON(){
   readButton_1 = digitalRead(butLight1_Pin);
@@ -64,32 +104,16 @@ void buttonON(){
     lastButState_2 = readButton_2;
     if((lastButState_1 == LOW) || (lastButState_2 == LOW)){
       lightState = !lightState;
-      int val = adjLamp;
       if(lightState==LOW){
-        do{
-          delay(dlyLamp);
-          val = val-1;
-          analogWrite(lpwm_Pin,val);
-        }while (val<1);
-        digitalWrite(len_Pin,lightState); //Enable L channel
+        digitalWrite(len_Pin,lightState); //Disable L channel
         digitalWrite(ren_Pin,lightState); //Disable R channel
       }
       if(lightState==HIGH){
         digitalWrite(len_Pin,lightState); //Enable L channel
-        digitalWrite(ren_Pin,lightState); //Disable R channel
-        do{
-          delay(dlyLamp);
-          val = val+1;
-          analogWrite(lpwm_Pin,val);
-        }while (val<1);
+        digitalWrite(ren_Pin,lightState); //Enable R channel
       }
     }
   }
-
-
-  Serial.print("lightState: ");
-  Serial.println(lightState);
-
 }
 
 //Current Driver Read
@@ -98,46 +122,14 @@ void currentRead(){
   ris = analogRead(ris_Pin);
   Serial.print(lis);
   Serial.print(" ");
-  Serial.print(ris);
+  Serial.println(ris);
 }
 
 void adjustLamp(){
   adjLamp = analogRead(potLine_Pin);
   adjLamp = map(adjLamp,0,1023,255,0);
   analogWrite(lpwm_Pin,adjLamp);
-  if (adjLamp < 51) {
-    digitalWrite(8,HIGH);
-    for (int i=9; i<13; i++){
-      digitalWrite(i,LOW);
-    }
-  }
-  if (adjLamp > 50 && adjLamp < 102){
-     for (int i=8; i<10; i++){
-      digitalWrite(i,HIGH);
-    }
-    for (int i=10; i<13; i++){
-      digitalWrite(i,LOW);
-    }
-  }
-  if (adjLamp > 101 && adjLamp < 153){
-     for (int i=8; i<11; i++){
-      digitalWrite(i,HIGH);
-    }
-    for (int i=11; i<13; i++){
-      digitalWrite(i,LOW);
-    }
-  }
-  if (adjLamp > 152 && adjLamp < 204){
-     for (int i=8; i<12; i++){
-      digitalWrite(i,HIGH);
-    }
-    digitalWrite(12,LOW);
-  }
-  if (adjLamp > 203 && adjLamp < 256){
-     for (int i=8; i<13; i++){
-      digitalWrite(i,HIGH);
-    }
-  }
+  currentLed(adjLamp);
 }
 
 void setup() {
@@ -154,9 +146,10 @@ void setup() {
   for (int i=7; i<16; i++){
     digitalWrite(i,LOW);
   }
-  digitalWrite(len_Pin,HIGH); //Enable L channel
-  digitalWrite(ren_Pin,HIGH); //Disable R channel
+  digitalWrite(len_Pin,LOW); //Enable L channel
+  digitalWrite(ren_Pin,LOW); //Disable R channel
   analogWrite(rpwm_Pin,0); //R Channel Off from PWM control
+  digitalWrite(led0_Pin,HIGH);
 
   //Status of Microscope ON
   blinkLed();
@@ -164,8 +157,11 @@ void setup() {
 
 
 void loop() {
-  adjustLamp();
   buttonON();
-  //currentRead();
-  delay(200); 
+  adjustLamp();
+  currentRead();
+  //delay(100); 
+  if(lightState==LOW){
+    standbyBlink();
+  }
 }
